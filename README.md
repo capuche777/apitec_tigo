1a256bb3f55706a5db7e5066b43c8447637258b1
Jeremias Enriquez <capuche777@gmail.com>
2018-01-26 12:17:33 -0600

    Plan1 has been finished

---
+++ README.md
@@ -0,0 +1,14 @@
+# TIGO #
+
+Minutos - Datos - Tiempo - Precio
+
+- Plan 1 = 100 - 100 - 10 - Q100
+- Plan 2 = 200 - 1000 - 100 - Q150
+- Plan 3 = Unlimited - 10000 - Unlimited - Q200
+
+------
+
+- Datos = Q1/mb
+- Mensajes = Q0.05/mn
+- Paquetes = Q25/mes
+- Llamadas = Q4/min
---
+++ tigo.js
@@ -0,0 +1,25 @@
+alert ('Bienvenido al sistema de pagos\n TIGO');
+
+var plan_usuario = Number(prompt('¿Cual es tu paquete de datos?'));
+var minutos_consumidos = Number(prompt('¿Cuantos minutos has consumido de tu paquete?'));
+var datos_consumidos = Number(prompt('¿Cuantos datos has consumido de tu paquete?'));
+var mensajes_enviados = Number(prompt('¿Cuantos mensajes has enviado?'));
+var paquetes_adquiridos = confirm('Has adiquirido algún paquete');
+var total = 0;
+
+
+if (plan_usuario == 1) {
+    if (minutos_consumidos > 100) {
+        total = (total + (minutos_consumidos - 100) * 4);
+        if (datos_consumidos > 100) {
+            total = (total + (datos_consumidos - 100));
+            if (mensajes_enviados > 10) {
+                total = (total + (mensajes_enviados - 10)* 0.5);
+                if (paquetes_adquiridos) {
+                    total = (total + 25)
+                }
+            }
+        }
+    }
+    alert(`El total a pagar es:\n Q.${total}`);
+}
